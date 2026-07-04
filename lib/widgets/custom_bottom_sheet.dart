cf2d7b722c6696332e2ace6fd957a1c3273648c7
EhabMohamed <ehab.mo.work@gmail.com>
2026-07-04 18:02:08 +0300

    add text fields to Bottom sheet

--- lib/widgets/custom_bottom_sheet.dart
+++ lib/widgets/custom_bottom_sheet.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:notes2_app/widgets/custom_text_field.dart';
 
 class CustomBottomSheet extends StatelessWidget {
   const CustomBottomSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      child: Column(
+        children: const [
+          CustomTextField(lableText: 'Title'),
+          SizedBox(height: 24),
+          CustomTextField(lableText: 'Content', maxLines: 4),
+          Spacer(),
+        ],
+      ),
+    );
   }
 }
---
+++ lib/widgets/custom_text_field.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  const CustomTextField({
+    super.key,
+    required this.lableText,
+    this.contentPadding = 24,
+    this.maxLines = 1,
+  });
+  final String lableText;
+  final double contentPadding;
+  final int maxLines;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      maxLines: maxLines,
+      cursorColor: Colors.white,
+
+      decoration: InputDecoration(
+        contentPadding: EdgeInsets.all(contentPadding),
+        labelText: lableText,
+        labelStyle: TextStyle(color: Colors.white),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+          borderSide: BorderSide(color: Colors.white),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+          borderSide: BorderSide(color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
