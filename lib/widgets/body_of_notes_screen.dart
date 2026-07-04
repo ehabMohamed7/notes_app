20e4ab1e29dd49877b0a0f4836493d4b93eb9910
EhabMohamed <ehab.mo.work@gmail.com>
2026-07-04 15:44:55 +0300

    Notes list view

--- lib/widgets/body_of_notes_screen.dart
+++ lib/widgets/body_of_notes_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:notes2_app/widgets/custom_appbar.dart';
+import 'package:notes2_app/widgets/note_item.dart';
 
 class BodyOfNotesScreen extends StatelessWidget {
   const BodyOfNotesScreen({super.key});
@@ -8,7 +9,27 @@ class BodyOfNotesScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: Column(children: const [SizedBox(height: 50), CustomAppBar()]),
+      child: Column(
+        children: const [SizedBox(height: 50), CustomAppBar(), NotesListView()],
+      ),
+    );
+  }
+}
+
+class NotesListView extends StatelessWidget {
+  const NotesListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return Padding(
+            padding: const EdgeInsets.only(bottom: 16),
+            child: NoteItem(),
+          );
+        },
+      ),
     );
   }
 }
