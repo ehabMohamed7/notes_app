d612edf1bced9571f058688ec22270457308f929
EhabMohamed <ehab.mo.work@gmail.com>
2026-07-04 15:51:01 +0300

    add FloatingActionButton

--- lib/screens/notes_screen.dart
+++ lib/screens/notes_screen.dart
@@ -6,6 +6,13 @@ class NotesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: BodyOfNotesScreen());
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(26)),
+        child: Icon(Icons.add),
+      ),
+      body: BodyOfNotesScreen(),
+    );
   }
 }
--- lib/widgets/body_of_notes_screen.dart
+++ lib/widgets/body_of_notes_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:notes2_app/widgets/custom_appbar.dart';
-import 'package:notes2_app/widgets/note_item.dart';
+import 'package:notes2_app/widgets/notes_list_view.dart';
 
 class BodyOfNotesScreen extends StatelessWidget {
   const BodyOfNotesScreen({super.key});
@@ -11,24 +11,6 @@ class BodyOfNotesScreen extends StatelessWidget {
       padding: const EdgeInsets.symmetric(horizontal: 16),
       child: Column(
         children: const [SizedBox(height: 50), CustomAppBar(), NotesListView()],
-      ),
-    );
-  }
-}
-
-class NotesListView extends StatelessWidget {
-  const NotesListView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(
-      child: ListView.builder(
-        itemBuilder: (ctx, index) {
-          return Padding(
-            padding: const EdgeInsets.only(bottom: 16),
-            child: NoteItem(),
-          );
-        },
       ),
     );
   }
