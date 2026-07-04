9f8fbc29640152de7fb8cb065a47c9c1a4d56904
EhabMohamed <ehab.mo.work@gmail.com>
2026-07-04 15:28:51 +0300

    creates custom  note item

---
+++ lib/widgets/note_item.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class NoteItem extends StatelessWidget {
+  const NoteItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 24),
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: Colors.yellow,
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+            title: Text(
+              'Flutter tips',
+              style: TextStyle(color: Colors.black, fontSize: 30),
+            ),
+
+            subtitle: Text(
+              'Welcome to this app \n with ehab',
+              style: TextStyle(color: Colors.black45, fontSize: 20),
+            ),
+
+            trailing: IconButton(
+              onPressed: () {
+                // TODO remember to implementation
+              },
+
+              icon: Icon(Icons.delete, color: Colors.black, size: 35),
+            ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.only(right: 30, top: 16),
+            child: Text(
+              'Jul 2,2026',
+              style: TextStyle(color: Colors.black45, fontSize: 20),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
